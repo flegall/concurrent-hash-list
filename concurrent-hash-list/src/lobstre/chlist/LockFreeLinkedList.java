315c26f150d118ea20c9b3413f817b9923abfc66
Florent Le Gall <florent.legall@gmail.com>
2012-12-02 18:50:54 +0100

    Added flag in structure

--- concurrent-hash-list/src/lobstre/chlist/LockFreeLinkedList.java
+++ concurrent-hash-list/src/lobstre/chlist/LockFreeLinkedList.java
@@ -1,21 +1,62 @@
 package lobstre.chlist;
 
-import java.util.concurrent.atomic.AtomicMarkableReference;
-
 public class LockFreeLinkedList {
 	
 	static class Node<K, V> {
-		public Node (K key, V value) {
+		public Node (final K key, final V value) {
 			this.key = key;
-			this.value = value;	
-			this.next = new AtomicMarkableReference<LockFreeLinkedList.Node<K,V>>(null, false);
+			this.value = value;
+			this.next = new NextLink<K, V> (null, false, false);
 			this.backlink = null;
+		}
+
+		public K getKey() {
+			return key;
+		}
+		
+		public V getValue() {
+			return value;
+		}
+		
+		public NextLink<K, V> getNext() {
+			return next;
+		}
+		
+		public Node<K, V> getBacklink() {
+			return backlink;
 		}
 
 		private final K key;
 		private final V value;
-		private final AtomicMarkableReference<Node<K, V>> next;
+		private volatile NextLink<K, V> next;
 		private final Node<K, V> backlink;
+	}
+	
+	static class NextLink<K, V> {
+		public NextLink(
+				final Node<K, V> node, 
+				final boolean marked, 
+				final boolean flagged) {
+			this.node = node;
+			this.marked = marked;
+			this.flagged = flagged;
+		}
+		
+		public Node<K, V> getNode() {
+			return node;
+		}
+		
+		public boolean isMarked() {
+			return marked;
+		}
+		
+		public boolean isFlagged() {
+			return flagged;
+		}
+		
+		private final Node<K, V> node;
+		private final boolean marked;
+		private final boolean flagged;
 	}
 }
 
