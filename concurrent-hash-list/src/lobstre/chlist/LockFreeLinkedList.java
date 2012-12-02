a0ebf9fb18b1ef5e715a018607928383e5db6e57
Florent Le Gall <florent.legall@gmail.com>
2012-12-02 17:00:34 +0100

    Added node structure

--- concurrent-hash-list/src/lobstre/chlist/LockFreeLinkedList.java
+++ concurrent-hash-list/src/lobstre/chlist/LockFreeLinkedList.java
@@ -1,5 +1,21 @@
 package lobstre.chlist;
 
-public class LockFreeLinkedList {
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
+public class LockFreeLinkedList {
+	
+	static class Node<K, V> {
+		public Node (K key, V value) {
+			this.key = key;
+			this.value = value;	
+			this.next = new AtomicMarkableReference<LockFreeLinkedList.Node<K,V>>(null, false);
+			this.backlink = null;
+		}
+
+		private final K key;
+		private final V value;
+		private final AtomicMarkableReference<Node<K, V>> next;
+		private final Node<K, V> backlink;
+	}
 }
+
